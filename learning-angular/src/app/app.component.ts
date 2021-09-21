f0bfbfaed8b10b59586ce3ce616d0de0cdafd482
amillerman <millerman.alex@gmail.com>
2021-09-21 14:18:50 -0400

    Revert "FInished class for week 3"
    This reverts commit b54b037c722ccfc30d9bece5c06ab7827a06b9f2.

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ContentList } from 'helper-files/ContentList';
 import { MarvelHero } from 'helper-files/MarvelHero';
 
@@ -7,37 +7,37 @@ import { MarvelHero } from 'helper-files/MarvelHero';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public title = "learning-angular";
-  contentList: MarvelHero[] = [{
-    id: 0,
-    name: "Spiderman",
-    imageURL: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
-    description: "Obviously the best marvel hero"
-  },
-  {
-    id: 0,
-    name: "Ironman",
-    imageURL: "https://m.media-amazon.com/images/I/71lVAGaqBtL._AC_SL1000_.jpg",
-    description: "We will miss him"
-  },
-  {
-    id: 0,
-    name: "Spiderman",
-    imageURL: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
-    description: "Obviously the best marvel hero"
-  },
-  {
-    id: 0,
-    name: "Ironman",
-    imageURL: "https://m.media-amazon.com/images/I/71lVAGaqBtL._AC_SL1000_.jpg",
-    description: "We will miss him"
-  }];
 
-  public constructor() {
-
-  }
 
   public ngOnInit(): void {
+
+    let name: string;
+    name = "peter";
+    console.log("Name is", name);
+
+    let contentItem: MarvelHero = {
+      id: 0,
+      name: "Spiderman",
+      imageURL: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+      description: "Obviously the best marvel hero"
+    };
+    this.processHero(contentItem);
+
+    // ContentList.contentCount = 1;
+    let list: ContentList;
+    list = new ContentList(contentItem);
+    list.items = [{
+      id: 0,
+      name: "Ironman",
+      imageURL: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+      description: "We will miss him"
+    }];
+    console.log("We changed the hero on the inside", list.items);
+
   };
+  public processHero(hero: MarvelHero): void {
+    console.log(hero.description);
+  }
 }
