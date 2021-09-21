b54b037c722ccfc30d9bece5c06ab7827a06b9f2
amillerman <millerman.alex@gmail.com>
2021-09-21 14:18:01 -0400

    FInished class for week 3

---
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MarvelHero } from 'helper-files/MarvelHero';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+  @Input() content: any;
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
