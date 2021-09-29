23661d1ed94eb4ad8e4d1c7ac3b8232e41cb937c
amillerman <millerman.alex@gmail.com>
2021-09-29 11:40:27 -0400

    Completed assignment 2

--- MusicSite/src/app/content-card/content-card.component.ts
+++ MusicSite/src/app/content-card/content-card.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,41 +7,15 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-  public musicList: ContentList;
+  @Input() song: any;
   constructor() {
-    let newContent: Content;
-    this.musicList = new ContentList();
-
-    newContent = {
-      id: 0,
-      author: 'Dragonforce',
-      imgUrl: 'https://upload.wikimedia.org/wikipedia/en/f/f6/Through_the_Fire_and_Flames_Cover_Art.jpg',
-      type: 'power metal',
-      title: 'Through The Fire And The Flames',
-      body: 'A legendary song with too many solos',
-      tags: ['gh3', 'double solos', 'epic']
-    };
-    this.musicList.add(newContent);
-    newContent = {
-      id: 1,
-      author: 'Metallica',
-      type: 'metal',
-      title: 'Enter the Sandman',
-      body: 'This one was really hard on drums on rock band',
-    };
-    this.musicList.add(newContent);
-    newContent = {
-      id: 2,
-      author: 'Ed Sheeran',
-      title: 'Perfect',
-      body: 'Actually really talented',
-      tags: ['relationship goals']
-    };
-    this.musicList.add(newContent);
-
+    this.song = {};
   }
 
   ngOnInit(): void {
+  }
+  showId(): void {
+    console.log(this.song.id);
   }
 
 }
