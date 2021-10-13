94688d2c3c72bf248fc005f2b353321f7c6cce14
amillerman <millerman.alex@gmail.com>
2021-10-13 11:37:15 -0400

    Completed class example

--- MusicSite/src/app/app.module.ts
+++ MusicSite/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { TypeFilterPipe } from './type-filter.pipe';
+import { HoverStyleDirective } from './hover-style.directive';
+import { HoverCardDirective } from './hover-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    TypeFilterPipe
+    TypeFilterPipe,
+    HoverStyleDirective,
+    HoverCardDirective
   ],
   imports: [
     BrowserModule
